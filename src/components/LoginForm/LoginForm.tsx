0ac1dd44f1f37a9d772c01bedd5c627e156cbe2f
AlbertMosella <mosella.albert@gmail.com>
2022-05-31 14:46:25 +0200

    Added LoginForm Component and it's styles

---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+/* import { useDispatch } from "react-redux"; */
+import LoginFormStyle from "./LoginFormStyle";
+
+const LoginForm = (): JSX.Element => {
+  /* const dispatch = useDispatch(); */
+
+  const initialFormValue = {
+    username: "",
+    password: "",
+  };
+
+  const [formValues, setFormValues] = useState(initialFormValue);
+
+  const handleInputChange = (event: {
+    target: { id: string; value: string };
+  }) => {
+    setFormValues({ ...formValues, [event.target.id]: event.target.value });
+  };
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    if (formValues.username === "" || formValues.password === "") {
+      return;
+    }
+    /* dispatch(loginThunk(formValues)); */
+    setFormValues(initialFormValue);
+  };
+  return (
+    <>
+      <LoginFormStyle>
+        <form onSubmit={handleSubmit} noValidate>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={formValues.username}
+            onChange={handleInputChange}
+            autoComplete="off"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={formValues.password}
+            onChange={handleInputChange}
+            autoComplete="off"
+          />
+          <button className="form-button" type="submit">
+            Login
+          </button>
+        </form>
+      </LoginFormStyle>
+      ;
+    </>
+  );
+};
+
+export default LoginForm;
