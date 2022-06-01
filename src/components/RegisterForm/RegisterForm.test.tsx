2857333c6012fa4c2200477bf9d570248f0923e2
AlbertMosella <mosella.albert@gmail.com>
2022-06-01 10:47:44 +0200

    Added test to register form

--- src/components/RegisterForm/RegisterForm.test.tsx
+++ src/components/RegisterForm/RegisterForm.test.tsx
@@ -2,23 +2,25 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import store from "../../redux/store/store";
-import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
 
 describe("Given the RegisterForm component", () => {
   const inputText = "Nois";
   const labelTextPassword = "Password";
   const labelTextUsername = "Username";
+  const labelTextName = "Name";
 
   describe("When it's invoked", () => {
-    test("Then it should render the labels 'Username' and 'Password'", () => {
+    test("Then it should render the labels 'Username','Password' and 'Name'", () => {
       render(
         <Provider store={store}>
-          <LoginForm />
+          <RegisterForm />
         </Provider>
       );
 
       expect(screen.getAllByText("Username")).toHaveLength(1);
       expect(screen.getAllByText("Password")).toHaveLength(1);
+      expect(screen.getAllByText("Name")).toHaveLength(1);
     });
   });
 
@@ -26,14 +28,16 @@ describe("Given the RegisterForm component", () => {
     test("Then the value of the username input field should be 'Nois'", () => {
       render(
         <Provider store={store}>
-          <LoginForm />
+          <RegisterForm />
         </Provider>
       );
 
       userEvent.type(screen.getByLabelText(labelTextUsername), inputText);
       userEvent.type(screen.getByLabelText(labelTextPassword), inputText);
+      userEvent.type(screen.getByLabelText(labelTextName), inputText);
       expect(screen.getByLabelText(labelTextUsername)).toHaveValue(inputText);
       expect(screen.getByLabelText(labelTextUsername)).toHaveValue(inputText);
+      expect(screen.getByLabelText(labelTextName)).toHaveValue(inputText);
     });
   });
 
@@ -41,16 +45,18 @@ describe("Given the RegisterForm component", () => {
     test("Then the value of the username and password input fields should be empty", () => {
       render(
         <Provider store={store}>
-          <LoginForm />
+          <RegisterForm />
         </Provider>
       );
 
       userEvent.type(screen.getByLabelText(labelTextPassword), inputText);
       userEvent.type(screen.getByLabelText(labelTextUsername), inputText);
+      userEvent.type(screen.getByLabelText(labelTextName), inputText);
       const button = screen.getByRole("button");
       userEvent.click(button);
       expect(screen.getByLabelText(labelTextPassword)).toHaveValue("");
       expect(screen.getByLabelText(labelTextUsername)).toHaveValue("");
+      expect(screen.getByLabelText(labelTextName)).toHaveValue("");
     });
   });
 });
