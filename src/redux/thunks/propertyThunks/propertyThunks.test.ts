cd8e6d463c4e16f9a95298fcb7d4bf7be6b2d765
AlbertMosella <mosella.albert@gmail.com>
2022-06-03 21:27:35 +0200

    Added test to get thunk

---
+++ src/redux/thunks/propertyThunks/propertyThunks.test.ts
@@ -0,0 +1,46 @@
+import { loadPropertiesThunk } from "./propertyThunks";
+import { server } from "../mocks/server/server";
+import { loadAllPropertiesActionCreator } from "../../features/propertySlice";
+
+beforeEach(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe("Given a loadPropertiesThunk function", () => {
+  describe("When it is called", () => {
+    test("It should dispatch loadAllPropertiesActionCreator with api's data", async () => {
+      const dispatch = jest.fn();
+      const expectedData = [
+        {
+          typeOf: "House",
+          adress: "Street 1",
+          name: "Cal Pepe",
+          surface: 150,
+          price: 450000,
+          bedrooms: 3,
+          bathrooms: 2,
+          year: 1988,
+          description: "Casa to guapa",
+          image: "casatoguapa.jpg",
+          views: false,
+          airConditioning: true,
+          heating: true,
+          parking: false,
+          pool: true,
+          fireplace: false,
+          garden: true,
+          laundryRoom: false,
+          storage: true,
+          terrace: true,
+        },
+      ];
+
+      const expectedAction = loadAllPropertiesActionCreator(expectedData);
+
+      const thunk = loadPropertiesThunk();
+      await thunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalledWith(expectedAction);
+    });
+  });
+});
