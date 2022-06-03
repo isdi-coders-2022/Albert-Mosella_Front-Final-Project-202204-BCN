0ddde591cbdd820d9c0535ca4c154e0702e5c9d6
AlbertMosella <mosella.albert@gmail.com>
2022-06-03 21:54:41 +0200

    Added integration test to App

---
+++ src/App.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import store from "./redux/store/store";
+
+describe("Given the Property component", () => {
+  describe("When it's invoked", () => {
+    test("Then it should render a list element", () => {
+      const expectedLenght = 3;
+
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const result = screen.getAllByRole("listitem");
+
+      expect(result).toHaveLength(expectedLenght);
+    });
+  });
+});
