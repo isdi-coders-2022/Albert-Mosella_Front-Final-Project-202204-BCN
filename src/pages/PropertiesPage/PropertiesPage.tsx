41619579c039d92b6e297f59bf4e2d7b19e0baf2
AlbertMosella <mosella.albert@gmail.com>
2022-06-06 16:25:15 +0200

    Added routes to pages if logged

--- src/pages/PropertiesPage/PropertiesPage.tsx
+++ src/pages/PropertiesPage/PropertiesPage.tsx
@@ -3,13 +3,17 @@ import PropertiesList from "../../components/PropertiesList/PropertiesList";
 import PropertiesPageContainer from "./PropertiesPageStyles";
 
 const PropertiesPage = (): JSX.Element => {
+  const token = localStorage.getItem("token");
+
   return (
     <PropertiesPageContainer>
       <h2>All properties</h2>
       <div className="decoration-line"></div>
-      <NavLink to="/propertyform">
-        <button className="long-button">Add a new property</button>
-      </NavLink>
+      {token && (
+        <NavLink to="/propertyform">
+          <button className="long-button">Add a new property</button>
+        </NavLink>
+      )}
       <PropertiesList />
     </PropertiesPageContainer>
   );
--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,14 +1,21 @@
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const RegisterPageStyle = styled.div`
+  text-align: center;
+
+  a {
+    text-decoration: none;
+  }
+`;
 
 const RegisterPage = (): JSX.Element => {
-  const RegisterPageStyle = styled.div`
-    text-align: center;
-
-    a {
-      text-decoration: none;
-    }
-  `;
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  if (token) {
+    navigate("/allproperties");
+  }
 
   return (
     <RegisterPageStyle>
