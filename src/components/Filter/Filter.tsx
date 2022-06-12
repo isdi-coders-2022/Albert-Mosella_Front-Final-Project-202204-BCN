c1dba09172bb1ee0faa859838b151cee690b65f6
AlbertMosella <mosella.albert@gmail.com>
2022-06-12 10:08:30 +0200

    Refactor to filter component

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -26,36 +26,34 @@ const Filter = () => {
     setFilterProperties(allProperties);
   }, [allProperties]);
 
-  const filterByPrice = (option: number) => {
+  const priceFilter = (option: number): IProperty[] => {
+    const filterResult = allProperties.filter(
+      (property) => property.price < option
+    );
+    return filterResult;
+  };
+
+  const filterOption = (option: number) => {
+    deployPriceOptions();
     let filteredByPriceProperties = [];
     switch (option) {
       case 0:
         filteredByPriceProperties = allProperties;
         break;
-      case 1:
-        filteredByPriceProperties = allProperties.filter(
-          (property) => property.price < 200000
-        );
+      case 200000:
+        filteredByPriceProperties = priceFilter(option);
         break;
-      case 2:
-        filteredByPriceProperties = allProperties.filter(
-          (property) => property.price < 400000
-        );
+      case 400000:
+        filteredByPriceProperties = priceFilter(option);
         break;
-      case 3:
-        filteredByPriceProperties = allProperties.filter(
-          (property) => property.price < 600000
-        );
+      case 600000:
+        filteredByPriceProperties = priceFilter(option);
         break;
-      case 4:
-        filteredByPriceProperties = allProperties.filter(
-          (property) => property.price < 800000
-        );
+      case 800000:
+        filteredByPriceProperties = priceFilter(option);
         break;
-      case 5:
-        filteredByPriceProperties = allProperties.filter(
-          (property) => property.price < 1000000
-        );
+      case 1000000:
+        filteredByPriceProperties = priceFilter(option);
         break;
       default:
         filteredByPriceProperties = allProperties;
@@ -79,22 +77,37 @@ const Filter = () => {
               : "filter-options-container--hidden"
           }
         >
-          <button onClick={() => filterByPrice(0)} className="filter-options">
+          <button onClick={() => filterOption(0)} className="filter-options">
             Show All
           </button>
-          <button onClick={() => filterByPrice(1)} className="filter-options">
+          <button
+            onClick={() => filterOption(200000)}
+            className="filter-options"
+          >
             Under 200.000€
           </button>
-          <button onClick={() => filterByPrice(2)} className="filter-options">
+          <button
+            onClick={() => filterOption(400000)}
+            className="filter-options"
+          >
             Under 400.000€
           </button>
-          <button onClick={() => filterByPrice(3)} className="filter-options">
+          <button
+            onClick={() => filterOption(600000)}
+            className="filter-options"
+          >
             Under 600.000€
           </button>
-          <button onClick={() => filterByPrice(4)} className="filter-options">
+          <button
+            onClick={() => filterOption(800000)}
+            className="filter-options"
+          >
             Under 800.000€
           </button>
-          <button onClick={() => filterByPrice(5)} className="filter-options">
+          <button
+            onClick={() => filterOption(1000000)}
+            className="filter-options"
+          >
             Under 1.000.000€
           </button>
         </div>
