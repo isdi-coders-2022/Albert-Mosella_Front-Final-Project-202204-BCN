d022c95fae72c9d8cb94b026e56a7b08920c3142
AlbertMosella <mosella.albert@gmail.com>
2022-06-15 20:51:58 +0200

    Deleted redirect loginpage test

--- src/pages/LoginPage/LoginPage.test.tsx
+++ src/pages/LoginPage/LoginPage.test.tsx
@@ -4,13 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import store from "../../redux/store/store";
 import LoginPage from "./LoginPage";
 
-const mockNavigate = jest.fn();
-
-jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
-  useNavigate: () => mockNavigate,
-}));
-
 describe("Given a LoginPage Component", () => {
   describe("When it's invoked", () => {
     test("Then it should render the text 'User Login'", () => {
@@ -27,22 +20,6 @@ describe("Given a LoginPage Component", () => {
       const receivedResult = screen.getByText(expectedResult);
 
       expect(receivedResult).toBeInTheDocument();
-    });
-  });
-
-  describe("When it's invoked and the user is logged", () => {
-    test("Then it should call navigate", () => {
-      jest.spyOn(Storage.prototype, "getItem").mockReturnValue("token");
-
-      render(
-        <Provider store={store}>
-          <BrowserRouter>
-            <LoginPage />
-          </BrowserRouter>
-        </Provider>
-      );
-
-      expect(mockNavigate).toHaveBeenCalled();
     });
   });
 });
