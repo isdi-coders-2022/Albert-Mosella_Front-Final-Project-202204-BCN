1a64946ce713b3a68e4f960bfa72b4e74ae2c4c4
AlbertMosella <mosella.albert@gmail.com>
2022-06-15 20:42:46 +0200

    Added redirect navigate if logged

--- src/pages/EditPropertyPage/EditPropertyPage.test.tsx
+++ src/pages/EditPropertyPage/EditPropertyPage.test.tsx
@@ -4,6 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import store from "../../redux/store/store";
 import EditPropertyPage from "./EditPropertyPage";
 
+const mockNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
+
 describe("Given a EditPropertyPage Component", () => {
   describe("When it's invoked", () => {
     test("Then it should render the text 'Edit a property'", () => {
@@ -20,6 +27,22 @@ describe("Given a EditPropertyPage Component", () => {
       const receivedResult = screen.getByText(expectedResult);
 
       expect(receivedResult).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's invoked and the user is logged", () => {
+    test("Then it should call navigate", () => {
+      jest.spyOn(Storage.prototype, "getItem").mockReturnValue(null);
+
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <EditPropertyPage />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      expect(mockNavigate).toHaveBeenCalled();
     });
   });
 });
--- src/pages/PropertyFormPage/PropertyFormPage.test.tsx
+++ src/pages/PropertyFormPage/PropertyFormPage.test.tsx
@@ -4,6 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import store from "../../redux/store/store";
 import PropertyFormPage from "./PropertyFormPage";
 
+const mockNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
+
 describe("Given a PropertyFormPage Component", () => {
   describe("When it's invoked", () => {
     test("Then it should render the text 'Create a property'", () => {
@@ -20,6 +27,22 @@ describe("Given a PropertyFormPage Component", () => {
       const receivedResult = screen.getByText(expectedResult);
 
       expect(receivedResult).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's invoked and the user is logged", () => {
+    test("Then it should call navigate", () => {
+      jest.spyOn(Storage.prototype, "getItem").mockReturnValue(null);
+
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <PropertyFormPage />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      expect(mockNavigate).toHaveBeenCalled();
     });
   });
 });
